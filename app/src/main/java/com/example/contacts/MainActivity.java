e9af910d2eb3197d0c17aebd14ed2a569f774838
Oleg-Storozhev <olegStoroshev@gmail.com>
2021-05-29 00:40:58 +0300

    added email and gender + created static class for randoms

--- app/src/main/java/com/example/contacts/MainActivity.java
+++ app/src/main/java/com/example/contacts/MainActivity.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,10 +27,12 @@ public class MainActivity extends AppCompatActivity {
     private GridView gridView;
     private ListViewAdapter listViewAdapter;
     private GridViewAdapter gridViewAdapter;
-    private List<Person> personList = new ArrayList<>();
+    private final List<Person> personList = new ArrayList<>();
     private int currentViewMode = 0;
-    private final String[] name = {"Oleg", "Igor", "Sergey", "Mike", "Jack", "Tonny", "Nick", "Mark", "Luke"};
+    // Random names list
+    private final String[] name_male = {"Oleg", "Igor", "Sergey", "Mike", "Jack", "Tonny", "Nick", "Mark", "Luke"};
     private final String[] surname = {"Storozhev", "Ivanov", "Sikorsky", "Tvist", "Green", "Tsivinskiy", "Melnik", "Vorotov", "Govologorov"};
+    private final String[] gender = {"male", "female"};
 
     static final int VIEW_MODE_LISTVIEW = 0;
     static final int VIEW_MODE_GRIDVIEW = 1;
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // setting everything
         stubList = findViewById(R.id.stub_list);
         stubGrid = findViewById(R.id.stub_grid);
 
@@ -48,20 +51,22 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         gridView = findViewById(R.id.gridView);
 
-        getPersonList();
+        getPersonList(); // make our 1st random list
 
         SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
         currentViewMode = sharedPreferences.getInt("currentViewMode", VIEW_MODE_LISTVIEW);
-
+        // setting ClickListeners for our two views
         listView.setOnItemClickListener(onItemClickListener);
         gridView.setOnItemClickListener(onItemClickListener);
 
-        Button btn1 = findViewById(R.id.random_button);
+        Button btn1 = findViewById(R.id.random_button); // button for random changes
         btn1.setOnClickListener(this :: RandomChanges);
 
-        switchView();
+        switchView(); // show the results on the screen
     }
-    public void RandomChanges(View view){
+
+    public void RandomChanges(View view){  // do random changes and clear old
+        personList.clear();
         getPersonList();
         switchView();
     }
@@ -87,35 +92,30 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public List<Person> getPersonList(){
-        int random_int = (int)Math.floor(Math.random()*25+5); // from 5 to 46
-
-        if(!personList.isEmpty())
-            personList.clear();
-
+    private void getPersonList(){
+        int random_int = (int)Math.floor(Math.random()*25+5); // from 5 to 25
         for(int i = 0; i < random_int; i++)
-            personList.add(new Person(R.drawable.avatar_icon, randomFullName(), getRandomBoolean()));
-
-        return personList;
+            personList.add(addPerson());
     }
 
-    public boolean getRandomBoolean() {
-        Random random = new Random();
-        return random.nextBoolean();
-    }
+    public Person addPerson(){
+        final String gender = RandomPerson.getRandomGender();
+        final String name = RandomPerson.getRandomName(gender); // save random name for email and full name
+        final String surname = RandomPerson.getRandomSurname(); // save random surname for email and full name
 
-    public String randomFullName(){
-        int first_name = (int) (Math.random()*9);
-        int second_name = (int) (Math.random()*9);
-        return name[first_name] + " " + surname[second_name];
+        // create strings for more readable code to input it on the method
+        final boolean online = RandomPerson.getRandomOnline();
+        final String fullName = name + " " + surname;
+        final String email = name.toLowerCase() + '.' + surname.toLowerCase() + "@gmail.com";
 
+        return new Person(R.drawable.avatar_icon, fullName, online, gender, email);
     }
 
     AdapterView.OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener(){
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-            // do smthing
-            Toast.makeText(getApplicationContext(),personList.get(position).getTitle() + " + " + personList.get(position).isOnline(), Toast.LENGTH_SHORT).show();
+            // do smthg
+            Toast.makeText(getApplicationContext(),personList.get(position).getTitle() + " + " + personList.get(position).isOnline()+ " + " + personList.get(position).getEmail() + " + " + personList.get(position).getGender(), Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -127,20 +127,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.item_menu_1:
-                if(VIEW_MODE_LISTVIEW == currentViewMode){
-                    currentViewMode = VIEW_MODE_GRIDVIEW;
-                } else{
-                    currentViewMode = VIEW_MODE_LISTVIEW;
-                }
-                switchView();
+        if (item.getItemId() == R.id.item_menu_1) {
+            if (VIEW_MODE_LISTVIEW == currentViewMode) {
+                currentViewMode = VIEW_MODE_GRIDVIEW;
+            } else {
+                currentViewMode = VIEW_MODE_LISTVIEW;
+            }
+            switchView();
 
-                SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putInt("currentViewMode", currentViewMode);
-                editor.commit();
-                break;
+            SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("currentViewMode", currentViewMode);
+            editor.apply();
         }
         return true;
     }
---
+++ app/src/main/java/com/example/contacts/RandomPerson.java
@@ -0,0 +1,33 @@
+package com.example.contacts;
+
+public class RandomPerson {
+    private static final String[] name_male = {"Oleg", "Igor", "Sergey", "Mike", "Jack", "Tonny", "Nick", "Mark", "Luke"};
+    private static final String[] name_female = {"Anna", "Nataliya", "Masha", "Alexandra", "Tanya", "Sonya", "Jessica", "Vera", "Olga"};
+    private static final String[] surname = {"Storozhev", "Ivanov", "Sikorsky", "Tvist", "Green", "Tsivinskiy", "Melnik", "Vorotov", "Govologorov"};
+    private static final String[] gender = {"Male", "Female"};
+
+    public static String getRandomName(String gender){
+        int random;
+        if(gender.equals("Male")) {
+            random = (int) (Math.random() * name_male.length);
+            return name_male[random];
+        }
+        else {
+            random = (int) (Math.random() * name_female.length);
+            return name_female[random];
+        }
+
+    }
+    public static String getRandomSurname(){
+        int random = (int) (Math.random()*surname.length);
+        return surname[random];
+    }
+    public static String getRandomGender(){
+        int random = (int) (Math.random()*2);
+        return gender[random];
+    }
+    public static boolean getRandomOnline(){
+        java.util.Random random = new java.util.Random();
+        return random.nextBoolean();
+    }
+}
